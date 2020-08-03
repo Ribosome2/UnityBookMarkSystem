d06abd216d0e1ef52078b69a72c912539cd4b156
qinshilin <qinshilin@aobi.com>
2020-08-03 17:43:14 +0800

    reformat

--- Assets/Editor/LinBookMark/LinBookMarkTreeView.cs
+++ Assets/Editor/LinBookMark/LinBookMarkTreeView.cs
@@ -19,89 +19,81 @@ namespace LinBookMark
 
         private void InitBookMarkList()
         {
-            var bookMark = new LinBookMarkElement(){name = "Root",depth = -1,id =TreeItemIdGenerator.NextId };
+            var bookMark = new LinBookMarkElement() {name = "Root", depth = -1, id = TreeItemIdGenerator.NextId};
             bookMarks.Add(bookMark);
-            var bookM2 = new LinBookMarkElement(){name = "customChild1",depth = 0 ,id = TreeItemIdGenerator.NextId };
+            var bookM2 = new LinBookMarkElement() {name = "customChild1", depth = 0, id = TreeItemIdGenerator.NextId};
             bookMarks.Add(bookM2);
-            bookMarks.Add( new LinBookMarkElement(){name = "Kyle ",depth = 1 ,id = TreeItemIdGenerator.NextId });
+            bookMarks.Add(new LinBookMarkElement() {name = "Kyle ", depth = 1, id = TreeItemIdGenerator.NextId});
         }
-        
-            public LinBookMarkTreeView(TreeViewState treeViewState)
-                : base(treeViewState)
-            {
-                InitBookMarkList();
-                bookMarkDataModel =new TreeModel<LinBookMarkElement>(bookMarks);
-                Reload();
-            }
-		
-  
-            
-            protected override TreeViewItem BuildRoot ()
-            {
-                // BuildRoot is called every time Reload is called to ensure
-                List<TreeViewItem> allItems =new List<TreeViewItem>();
-                var root = _treeBuilder.BuildRoot(bookMarkDataModel,allItems);
-                // Utility method that initializes the TreeViewItem.children and -parent for all items.
-                SetupParentsAndChildrenFromDepths (root, allItems);
-                return root;
-            }
-            
-            protected override bool CanStartDrag (CanStartDragArgs args)
-            {
-                return true;
-            }
-            
-            protected override void SetupDragAndDrop (SetupDragAndDropArgs args)
-            {
-                var sortedDraggedIDs = SortItemIDsInRowOrder (args.draggedItemIDs);
-                _dragDropHandler.SetupDragAndDrop(sortedDraggedIDs);
-            }
 
-    
-
-            protected override DragAndDropVisualMode HandleDragAndDrop (DragAndDropArgs args)
-            {
-                // First check if the dragged objects are GameObjects
-                var draggedObjects = DragAndDrop.objectReferences;
-                if (draggedObjects.Length == 0)
-                {
-                    return DragAndDropVisualMode.None;
-                }
+        public LinBookMarkTreeView(TreeViewState treeViewState)
+            : base(treeViewState)
+        {
+            InitBookMarkList();
+            bookMarkDataModel = new TreeModel<LinBookMarkElement>(bookMarks);
+            Reload();
+        }
 
 
-                // Reparent
-                if (args.performDrop)
-                {
-                    HandleDropOperation(args);
-                }
+        protected override TreeViewItem BuildRoot()
+        {
+            // BuildRoot is called every time Reload is called to ensure
+            List<TreeViewItem> allItems = new List<TreeViewItem>();
+            var root = _treeBuilder.BuildRoot(bookMarkDataModel, allItems);
+            // Utility method that initializes the TreeViewItem.children and -parent for all items.
+            SetupParentsAndChildrenFromDepths(root, allItems);
+            return root;
+        }
 
-                return DragAndDropVisualMode.Move;
-            }
+        protected override bool CanStartDrag(CanStartDragArgs args)
+        {
+            return true;
+        }
 
-            private void HandleDropOperation(DragAndDropArgs args)
+        protected override void SetupDragAndDrop(SetupDragAndDropArgs args)
+        {
+            var sortedDraggedIDs = SortItemIDsInRowOrder(args.draggedItemIDs);
+            _dragDropHandler.SetupDragAndDrop(sortedDraggedIDs);
+        }
+
+
+        protected override DragAndDropVisualMode HandleDragAndDrop(DragAndDropArgs args)
+        {
+            // First check if the dragged objects are GameObjects
+            var draggedObjects = DragAndDrop.objectReferences;
+            if (draggedObjects.Length == 0)
             {
-                switch (args.dragAndDropPosition)
-                {
-                    case DragAndDropPosition.UponItem:
-                    case DragAndDropPosition.BetweenItems:
-                        _dragDropHandler.HandleDropWithParentItem(args.insertAtIndex,args.parentItem,bookMarkDataModel);
-                        break;
-                        
-                    case DragAndDropPosition.OutsideItems:
-                        _dragDropHandler.HandleDropOutsideRoot(args.insertAtIndex,bookMarkDataModel);
-                        break;
-                    default:
-                        throw new ArgumentOutOfRangeException();
-                }
-                
-                Reload();
-                // SetSelection (transforms.Select (t => t.gameObject.GetInstanceID ()).ToList (), TreeViewSelectionOptions.RevealAndFrame);
+                return DragAndDropVisualMode.None;
             }
 
 
+            // Reparent
+            if (args.performDrop)
+            {
+                HandleDropOperation(args);
+            }
 
+            return DragAndDropVisualMode.Move;
+        }
 
+        private void HandleDropOperation(DragAndDropArgs args)
+        {
+            switch (args.dragAndDropPosition)
+            {
+                case DragAndDropPosition.UponItem:
+                case DragAndDropPosition.BetweenItems:
+                    _dragDropHandler.HandleDropWithParentItem(args.insertAtIndex, args.parentItem, bookMarkDataModel);
+                    break;
 
-   
+                case DragAndDropPosition.OutsideItems:
+                    _dragDropHandler.HandleDropOutsideRoot(args.insertAtIndex, bookMarkDataModel);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            Reload();
+            // SetSelection (transforms.Select (t => t.gameObject.GetInstanceID ()).ToList (), TreeViewSelectionOptions.RevealAndFrame);
+        }
     }
 }
